b0e5790aacc19ca4f87d9d74cc07a81411fe043c
Me-Harlddddd <agriamharold18@gmai.com>
2025-09-03 00:26:37 +0800

    Fixing register

--- public/build/assets/index-7BW4AZoS.js
+++
@@ -1,1 +0,0 @@
-import{g as r,d as t}from"./app-D4M_E8i_.js";var a=t();const e=r(a);export{e as R,a as r};
---
+++ public/build/assets/index-CPU8VbuK.js
@@ -0,0 +1,1 @@
+import{g as r,d as t}from"./app-C7YcC1ls.js";var a=t();const e=r(a);export{e as R,a as r};
